6553548079ee310edee84992c666b6c02acd95b1
musango willy <99553300+musango-willy@users.noreply.github.com>
2026-06-11 21:57:46 +0300

    Initialize sass

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+
+  sassOptions: {
+    prependData: `@import "./_mantine.scss";`,
+  },
+});
