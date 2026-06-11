60323362477612de3fb871bb28a8acadfcd18f2b
musango willy <99553300+musango-willy@users.noreply.github.com>
2026-06-11 21:57:50 +0300

    Initialize sass-embedded

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import path from 'path';
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer({
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+
+  sassOptions: {
+    implementation: 'sass-embedded',
+    additionalData: `@use "${path.join(process.cwd(), '_mantine')}" as mantine;`,
+  },
+});
